63d5f3d028eb5bf468b1f8bb24b91931bb2ca9b2
Fede <fede.atzeni372@gmail.com>
2025-06-27 15:27:18 +0200

    feat snack 2

--- index.test.js
+++ index.test.js
@@ -8,10 +8,21 @@ function getInitials(str) {
     return words.map(el => el.charAt(0).toUpperCase());
 }
 
-console.log(getInitials("Mario Rossi"));
-
-
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
     expect(getInitials("Mario Rossi")).toStrictEqual(["M", "R"]);
     expect(getInitials("mario Rossi")).toStrictEqual(["M", "R"]);
+})
+
+// Snack 2
+// Creare un test che verifichi la seguente descrizione:
+
+// "La funzione createSlug restituisce una stringa in lowercase."
+
+function createSlug(str) {
+    return str.toLowerCase()
+}
+
+test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+    expect(createSlug("Stringa")).toBe("stringa");
+    expect(createSlug("BLU")).toBe("blu");
 })
